7f25c5469acb1556921400533afeae31ee90b50b
Mitanshu Thakore <mitanshu.thakore@cern.ch>
2025-08-13 10:33:49 -0500

    Updated code

--- batch_signals_with_backgrounds.sh
+++ batch_signals_with_backgrounds.sh
@@ -1,42 +1,57 @@
-# PVs
-python3 norm.py --year 2024 --variables "PV_npvs"        --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_PV_npvs.txt &
-python3 norm.py --year 2024 --variables "PV_npvsGood"    --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_PV_npvsGood.txt &
+#!/usr/bin/env bash
+set -u
 
-# Leading FatJet
-python3 norm.py --year 2024 --variables "FatJet_pt[0]"   --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_FatJet_pt_leading.txt &
-python3 norm.py --year 2024 --variables "FatJet_eta[0]"  --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_FatJet_eta_leading.txt &
-python3 norm.py --year 2024 --variables "FatJet_phi[0]"  --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_FatJet_phi_leading.txt &
-python3 norm.py --year 2024 --variables "FatJet_mass[0]" --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_FatJet_mass_leading.txt &
-python3 norm.py --year 2024 --variables "FatJet_msoftdrop[0]" --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_FatJet_msoftdrop_leading.txt &
-python3 norm.py --year 2024 --variables "FatJet_particleNetLegacy_mass[0]" --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_FatJet_particleNetLegacy_mass_leading.txt &
+YEAR=2024
+LUMI=109.08
+WEIGHTS="genWeight"
+CHANNEL="all"
+MAX=1e8
 
-# Electrons
-python3 norm.py --year 2024 --variables "nElectron"      --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_nElectron.txt &
-python3 norm.py --year 2024 --variables "Electron_pt" --cuts "1" --weights "genWeight" --set_maximum 1e9 --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Electron_pt_leading.txt &
-python3 norm.py --year 2024 --variables "Electron_eta" --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Electron_eta_leading.txt &
-python3 norm.py --year 2024 --variables "Electron_phi" --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Electron_phi_leading.txt &
-python3 norm.py --year 2024 --variables "nFatJet" --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Electron_phi_leading.txt &
+# Common selection used everywhere
+CUTS='FatJet_msoftdrop > 30 && Tau_pt > 20 && abs(Tau_eta) < 2.5 && abs(Tau_dz) < 0.2 && abs(boostedTau_eta) < 2.5 && Electron_pt > 10 && abs(Electron_eta) < 2.4 && abs(Muon_eta) < 2.5 && Muon_pt > 15 && PuppiMET_pt > 100 && abs(FatJet_eta) < 2.5 && FatJet_pt > 200 && abs(Jet_eta) < 2.5 && Jet_pt > 30'
 
-# Muons
-python3 norm.py --year 2024 --variables "nMuon"          --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_nMuon.txt &
-python3 norm.py --year 2024 --variables "Muon_pt"     --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Muon_pt_leading.txt &
-python3 norm.py --year 2024 --variables "Muon_eta"    --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Muon_eta_leading.txt &
-python3 norm.py --year 2024 --variables "Muon_phi"    --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Muon_phi_leading.txt &
+# # --- PVs ---
+# python3 norm.py --year "$YEAR" --variables "PV_npvs"     --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_PV_npvs.txt &
+# python3 norm.py --year "$YEAR" --variables "PV_npvsGood" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_PV_npvsGood.txt &
 
-# Taus
-python3 norm.py --year 2024 --variables "nTau"           --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_nTau.txt &
-python3 norm.py --year 2024 --variables "Tau_pt"      --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Tau_pt_leading.txt &
+# --- FatJet ---
+python3 norm.py --year "$YEAR" --variables "nFatJet"                      --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_nFatJet.txt &
 
-# Boosted Taus
-python3 norm.py --year 2024 --variables "nboostedTau"    --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_nboostedTau.txt &
-python3 norm.py --year 2024 --variables "boostedTau_pt"  --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_boostedTau_pt.txt &
+# python3 norm.py --year "$YEAR" --variables "FatJet_pt"                      --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_FatJet_pt.txt &
+# python3 norm.py --year "$YEAR" --variables "FatJet_eta"                     --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_FatJet_eta.txt &
+# python3 norm.py --year "$YEAR" --variables "FatJet_phi"                     --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_FatJet_phi.txt &
+# python3 norm.py --year "$YEAR" --variables "FatJet_mass"                    --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_FatJet_mass.txt &
+# python3 norm.py --year "$YEAR" --variables "FatJet_msoftdrop"               --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_FatJet_msoftdrop.txt &
+# python3 norm.py --year "$YEAR" --variables "FatJet_particleNetLegacy_mass"  --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_FatJet_particleNetLegacy_mass.txt &
 
-# PuppiMET
-python3 norm.py --year 2024 --variables "PuppiMET_pt"    --cuts "1" --weights "genWeight" --Channel all --luminosity 109.08  &> log_2024_signal_with_background_PuppiMET_pt.txt &
-python3 norm.py --year 2024 --variables "PuppiMET_phi"   --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_PuppiMET_phi.txt &
+# # --- Electrons ---
+# python3 norm.py --year "$YEAR" --variables "nElectron"   --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_nElectron.txt &
+# python3 norm.py --year "$YEAR" --variables "Electron_pt" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Electron_pt.txt &
+# python3 norm.py --year "$YEAR" --variables "Electron_eta" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Electron_eta.txt &
+# python3 norm.py --year "$YEAR" --variables "Electron_phi" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Electron_phi.txt &
 
-# Leading AK4 Jet
-python3 norm.py --year 2024 --variables "nJet"           --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_nJet.txt &
-python3 norm.py --year 2024 --variables "Jet_pt[0]"      --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Jet_pt_leading.txt &
-python3 norm.py --year 2024 --variables "Jet_eta[0]"     --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Jet_eta_leading.txt &
-python3 norm.py --year 2024 --variables "Jet_phi[0]"     --cuts "1" --weights "genWeight" --set_maximum 1e9 --Channel all --log_scale --luminosity 109.08  &> log_2024_signal_with_background_Jet_phi_leading.txt &
+# # --- Muons ---
+# python3 norm.py --year "$YEAR" --variables "nMuon"    --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_nMuon.txt &
+# python3 norm.py --year "$YEAR" --variables "Muon_pt"  --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Muon_pt.txt &
+# python3 norm.py --year "$YEAR" --variables "Muon_eta" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Muon_eta.txt &
+# python3 norm.py --year "$YEAR" --variables "Muon_phi" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Muon_phi.txt &
+
+# # --- Taus ---
+# python3 norm.py --year "$YEAR" --variables "nTau"   --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_nTau.txt &
+# python3 norm.py --year "$YEAR" --variables "Tau_pt" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Tau_pt.txt &
+
+# # --- Boosted Taus ---
+# python3 norm.py --year "$YEAR" --variables "nboostedTau"  --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_nboostedTau.txt &
+# python3 norm.py --year "$YEAR" --variables "boostedTau_pt" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_boostedTau_pt.txt &
+
+# # --- PuppiMET ---
+# python3 norm.py --year "$YEAR" --variables "PuppiMET_pt"  --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_PuppiMET_pt.txt &
+# python3 norm.py --year "$YEAR" --variables "PuppiMET_phi" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_PuppiMET_phi.txt &
+
+# # --- AK4 Jets ---
+# python3 norm.py --year "$YEAR" --variables "nJet"   --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_nJet.txt &
+# python3 norm.py --year "$YEAR" --variables "Jet_pt" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Jet_pt.txt &
+# python3 norm.py --year "$YEAR" --variables "Jet_eta" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Jet_eta.txt &
+# python3 norm.py --year "$YEAR" --variables "Jet_phi" --cuts "$CUTS" --weights "$WEIGHTS" --set_maximum "$MAX" --Channel "$CHANNEL" --log_scale --luminosity "$LUMI" &> log_${YEAR}_signals_with_backgrounds_Jet_phi.txt &
+
+# echo "All jobs launched"
--- variable_dictionaries.py
+++ variable_dictionaries.py
@@ -1,43 +1,59 @@
 variableAxisTitleDictionary = {
     "PV_npvs": "Number of Primary Vertices",
-    "PV_npvsGood": "Number of Primary Vertices (Good)",    
+    "PV_npvsGood": "Number of Primary Vertices (Good)",
+
+    # All FatJets
+    "FatJet_pt": "All AK8 Jet Pt (GeV)",
+    "FatJet_eta": "All AK8 Jet #eta",
+    "FatJet_phi": "All AK8 Jet #phi",
+    "FatJet_mass": "All AK8 Jet mass (GeV)",
+    "FatJet_msoftdrop": "All AK8 Jet softdrop mass (GeV)",
+    "FatJet_particleNetLegacy_mass": "All AK8 Jet ParticleNet Legacy mass (GeV)",
+    "nFatJet": "Number of AK8 Jets", 
 
     # Leading FatJet
-    "FatJet_pt[0]": "Leading FatJet p_{T} [GeV]",
+    "FatJet_pt[0]": "Leading FatJet p_{T} (GeV)",
     "FatJet_eta[0]": "Leading FatJet #eta",
     "FatJet_phi[0]": "Leading FatJet #phi",
-    "FatJet_mass[0]": "Leading FatJet mass [GeV]",
-    "FatJet_msoftdrop[0]": "Leading FatJet soft drop mass (PUPPI) [GeV]",
-    "FatJet_particleNetLegacy_mass[0]": "Leading FatJet ParticleNet Legacy mass [GeV]",
+    "FatJet_mass[0]": "Leading FatJet mass (GeV)",
+    "FatJet_msoftdrop[0]": "Leading FatJet soft drop mass (PUPPI) (GeV)",
+    "FatJet_particleNetLegacy_mass[0]": "Leading FatJet ParticleNet Legacy mass (GeV)",
 
     # Electron
     "nElectron": "Number of Electrons",
-    "Electron_pt": "Electron p_{T} [GeV]",
+    "Electron_pt": "Electron p_{T} (GeV)",
     "Electron_eta": "Electron #eta",
     "Electron_phi": "Electron #phi",
-    "nFatJet": "Number of FatJets",
 
     # Muon
     "nMuon": "Number of Muons",
-    "Muon_pt": "Muon p_{T} [GeV]",
+    "Muon_pt": "Muon p_{T} (GeV)",
     "Muon_eta": "Muon #eta",
     "Muon_phi": "Muon #phi",
 
     # Taus
     "nTau": "Number of Taus (HPS)",
-    "Tau_pt": "Tau p_{T} [GeV]",
+    "Tau_pt": "Tau p_{T} (GeV)",
+    "Tau_eta": "Tau #eta",
+    "Tau_phi": "Tau #phi",
 
     # Boosted Taus
     "nboostedTau": "Number of Boosted Taus",
-    "boostedTau_pt": "Boosted Tau p_{T} [GeV]",
+    "boostedTau_pt": "Boosted Tau p_{T} (GeV)",
 
     # PuppiMET
     "PuppiMET_pt": "Puppi MET (GeV)",
     "PuppiMET_phi": "Puppi MET Phi",
+    
+    # AK4 Jets
+    "Jet_pt": "All AK4 (PUPPI) Jet p_{T} (GeV)",
+    "Jet_eta": "All AK4 (PUPPI) Jet #eta",
+    "Jet_phi": "All AK4 (PUPPI) Jet #phi",
+
 
     # Leading AK4 Jet
     "nJet": "Number of AK4 jets",
-    "Jet_pt[0]": "Leading AK4 Jet p_{T} [GeV]",
+    "Jet_pt[0]": "Leading AK4 Jet p_{T} (GeV)",
     "Jet_eta[0]": "Leading AK4 Jet #eta",
     "Jet_phi[0]": "Leading AK4 Jet #phi"
 }
@@ -46,8 +62,17 @@ variableFileNameDictionary = {
     "PV_npvs": "PVs",
     "PV_npvsGood": "goodPVs",
 
-    # Leading FatJet
+    # All FatJets
     "nFatJet": "nfatjets",
+    "FatJet_pt": "fatjet_pt",
+    "FatJet_eta": "fatjet_eta",
+    "FatJet_phi": "fatjet_phi",
+    "FatJet_mass": "fatjet_mass",
+    "FatJet_msoftdrop": "fatjet_msoftdrop",
+    "FatJet_particleNetLegacy_mass": "fatjet_particlenetlegacy_mass",
+
+    # Leading FatJet
+
     "FatJet_pt[0]": "fatjet_pt_leading",
     "FatJet_eta[0]": "fatjet_eta_leading",
     "FatJet_phi[0]": "fatjet_phi_leading",
@@ -70,6 +95,8 @@ variableFileNameDictionary = {
     # Taus
     "nTau": "ntaus_hps",
     "Tau_pt": "tau_pt",
+    "Tau_eta": "tau_eta",
+    "Tau_phi": "tau_phi",
 
     # Boosted Taus
     "nboostedTau": "nboostedtaus",
@@ -79,8 +106,14 @@ variableFileNameDictionary = {
     "PuppiMET_pt": "puppimet_pt",
     "PuppiMET_phi": "puppimet_phi",
 
-    # Leading AK4 Jet
+    # AK4 jets
     "nJet": "n_ak4jets",
+    "Jet_pt": "ak4_jet_pt",
+    "Jet_eta": "ak4_jet_eta",
+    "Jet_phi": "ak4_jet_phi",
+
+
+    # Leading AK4 Jet
     "Jet_pt[0]": "ak4_jet_pt_leading",
     "Jet_eta[0]": "ak4_jet_eta_leading",
     "Jet_phi[0]": "ak4_jet_phi_leading"
@@ -90,10 +123,18 @@ variableSettingDictionary = {
     "PV_npvs": "50,0,100",  
     "PV_npvsGood": "50,0,100",  
 
-    # Leading FatJet
+    # All FatJets
     "nFatJet": "10,0,10",
+    "FatJet_pt": "37,0,2050",      
+    "FatJet_eta": "16,-3.20,3.20",
+    "FatJet_phi": "16,-3.20,3.20",
+    "FatJet_mass": "25,0,250",              
+    "FatJet_msoftdrop": "25,0,250",
+    "FatJet_particleNetLegacy_mass": "25,0,250",
+
+    # Leading FatJet
     "FatJet_pt[0]": "37,0,2050",      
-    "FatJet_eta[0]": "16,-5,5",
+    "FatJet_eta[0]": "16,-3.20,3.20",
     "FatJet_phi[0]": "16,-3.2,3.2",
     "FatJet_mass[0]": "25,0,250",              
     "FatJet_msoftdrop[0]": "25,0,250",
@@ -101,31 +142,38 @@ variableSettingDictionary = {
 
     # Electron
     "nElectron": "20,0,20",
-    "Electron_pt": "20,0,500",
-    "Electron_eta": "16,-3,3",
-    "Electron_phi": "16,-3.2,3.2",
+    "Electron_pt": "40,0,400",
+    "Electron_eta": "16,-3.20,3.20",
+    "Electron_phi": "16,-3.20,3.20",
 
     # Muon
     "nMuon": "20,0,20",
-    "Muon_pt": "20,0,500",
-    "Muon_eta": "16,-3,3",
-    "Muon_phi": "16,-3.2,3.2",
+    "Muon_pt": "40,0,400",
+    "Muon_eta": "16,-3.20,3.20",
+    "Muon_phi": "16,-3.20,3.20",
 
     # Taus
     "nTau": "10,0,10",
-    "Tau_pt": "20,0,500",
+    "Tau_pt": "20,0,400",
+    "Tau_eta": "16,-3.20,3.20",
+    "Tau_phi": "16, -3.20,3.20",
 
     # Boosted Taus
     "nboostedTau": "10,0,10",
-    "boostedTau_pt": "20,0,500",
+    "boostedTau_pt": "20,0,400",
 
     # PuppiMET
-    "PuppiMET_pt": "21,0,1000",
+    "PuppiMET_pt": "19,50,1000",
     "PuppiMET_phi": "16,-3.20,3.20",
+    
+    # AK4 jets
+    "nJet": "20,0,20",
+    "Jet_pt": "20,0,400",
+    "Jet_eta": "16,-3.20,3.20",
+    "Jet_phi": "16,-3.20,3.20",
 
     # Leading AK4 Jet
-    "nJet": "20,0,20",
-    "Jet_pt[0]": "20,0,1000",
+    "Jet_pt[0]": "20,0,400",
     "Jet_eta[0]": "16,-3,3",
     "Jet_phi[0]": "16,-3.2,3.2"
 }
